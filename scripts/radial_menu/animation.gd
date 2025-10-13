19b9c1af9f0a8a5a1715e26386b6249828c299aa
puklol <237351810+puklol@users.noreply.github.com>
2025-10-13 14:08:55 +0300

    unify animations

--- scripts/radial_menu/animation.gd
+++ scripts/radial_menu/animation.gd
@@ -1,16 +1,16 @@
 extends AnimationPlayer
 
-var playing = false
+@export var anim:String
+var playing := false
 
 func _on_radial_menu_behavior_on_hower() -> void:
-	play("radial_menu_animations/sin")
+	play(anim)
 	playing = true
-
 
 func _on_radial_menu_behavior_on_unhower() -> void:
 	playing = false
 
-
 func _on_animation_finished(anim_name: StringName) -> void:
 	if playing:
-		play("radial_menu_animations/sin")
+		advance(0)
+		play(anim)
