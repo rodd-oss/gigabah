39a26439231e2e52f371912dba9eef1c6821b400
AleD93 <adworld.aled@gmail.com>
2025-10-13 16:40:52 +0900

    Attempt to implement multiplayer spawner with visibility control

---
+++ scripts/advanced_multiplayer_spawner.gd
@@ -0,0 +1,243 @@
+## Advanced version of MultiplayerSpawner which allows control visibility
+## of nodes. Nodes replicates only to those clients who allowed to see this
+## node.
+##
+## TODO: currently changing authority in middle of lifetime not supported
+## and can lead to undefined behaviour.
+class_name AdvancedMultiplayerSpawner
+extends Node
+
+var spawn_function: Callable ## must be func(data: Variant) -> Node
+@export var spawn_limit: int = 0
+@export var spawn_path: NodePath = "."
+@export var auto_spawnable_scenes: Array[PackedScene] = []
+var _watching_node: Node
+var _tracking_nodes: Dictionary[int, _NetworkNodeInfo] = {}
+
+signal spawned(node: Node)
+signal despawning(node: Node)
+
+func add_spawnable_scene(path: String) -> void:
+	auto_spawnable_scenes.append(load(path))
+
+func clear_spawnable_scenes() -> void:
+	auto_spawnable_scenes.clear()
+
+func get_spawnable_scene(index: int) -> String:
+	if index >= 0 && index < len(auto_spawnable_scenes):
+		return auto_spawnable_scenes[index].resource_path
+	return ""
+
+func get_spawnable_count() -> int:
+	return len(auto_spawnable_scenes)
+
+func spawn(data: Variant = null) -> Node:
+	if !is_multiplayer_authority():
+		printerr("attempt to call spawn method by non authority")
+		return null
+
+	if !spawn_function:
+		printerr("spawn_function must be set before calling spawn method")
+		return null
+
+	var node: Node = spawn_function.call(data)
+	if !node:
+		printerr("spawn_function doesn't returned node")
+		return null
+
+	var spawn_target: Node = get_node(spawn_path)
+	if !spawn_target:
+		printerr("spawn_path pointing to invalid node")
+		return null
+
+	spawn_target.add_child(node)
+
+	var net_node: _NetworkNodeInfo = _NetworkNodeInfo.new(_alloc_network_id(node))
+	_tracking_nodes[node.get_instance_id()] = net_node
+
+	return node
+
+func is_visible_for(peer_id: int, node: Node) -> bool:
+	# owner always see its own nodes
+	if node.get_multiplayer_authority() == peer_id:
+		return true
+
+	var net_node: _NetworkNodeInfo = _tracking_nodes.get(node.get_instance_id())
+	if !net_node:
+		return false
+
+	return peer_id in net_node.peers_vision
+
+## Set visibility of node for specific peer.
+## 
+## Note: you can't disable visibility for node owner
+func set_visibility_for(peer_id: int, node: Node, visibility: bool) -> void:
+	if node.get_multiplayer_authority() == peer_id:
+		return
+
+	var net_node: _NetworkNodeInfo = _tracking_nodes.get(node.get_instance_id())
+	if !net_node:
+		if visibility:
+			net_node = _NetworkNodeInfo.new(_alloc_network_id(node))
+			_tracking_nodes[node.get_instance_id()] = net_node
+			_on_peer_got_vision(node, net_node, peer_id)
+	else:
+		if visibility and peer_id not in net_node.peers_vision:
+			net_node.peers_vision.append(peer_id)
+			_on_peer_got_vision(node, net_node, peer_id)
+		elif !visibility and peer_id in net_node.peers_vision:
+			var idx: int = net_node.peers_vision.find(peer_id)
+			if idx >= 0:
+				_erase_replacing(net_node.peers_vision, idx)
+			_on_peer_lost_vision(node, net_node, peer_id)
+
+## Returns number of peers can see node, excluding owner
+func get_peers_have_vision_count(node: Node) -> int:
+	var net_node: _NetworkNodeInfo = _tracking_nodes.get(node.get_instance_id())
+	if net_node:
+		return net_node.peers_vision.size()
+	return 0
+
+## Get Nth peer id that can see node or -1 if index out of bounds
+func get_peer_have_vision(node: Node, index: int) -> int:
+	var net_node: _NetworkNodeInfo = _tracking_nodes.get(node.get_instance_id())
+	if net_node:
+		if index >= 0 && index < net_node.peers_vision.size():
+			return net_node.peers_vision[index]
+
+	return -1
+
+func _enter_tree() -> void:
+	if is_multiplayer_authority():
+		multiplayer.object_configuration_add(self, self)
+
+		_watching_node = get_node_or_null(spawn_path)
+		if _watching_node:
+			_watching_node.child_entered_tree.connect(_on_child_entered)
+			_watching_node.child_exiting_tree.connect(_on_child_exiting)
+
+func _exit_tree() -> void:
+	if is_multiplayer_authority():
+		multiplayer.object_configuration_remove(self, self)
+
+		if _watching_node:
+			_watching_node.child_entered_tree.connect(_on_child_entered)
+			_watching_node.child_exiting_tree.connect(_on_child_exiting)
+
+			_watching_node = null
+
+func _on_child_entered(node: Node) -> void:
+	var auto_spawn_scene: PackedScene = _try_get_auto_spawnable_scene(node)
+	if !auto_spawn_scene:
+		# not replicatable node or node that should be spawned manually
+		# using spawn method
+		return
+
+	print("Automatically replicating node %s (%d)" % [node.name, node.get_instance_id()])
+
+	var net_node: _NetworkNodeInfo = _tracking_nodes.get(node.get_instance_id())
+	if net_node:
+		# node already tracking
+		return
+
+	net_node = _NetworkNodeInfo.new(_alloc_network_id(node))
+	_tracking_nodes[node.get_instance_id()] = net_node
+
+func _on_child_exiting(node: Node) -> void:
+	var net_node: _NetworkNodeInfo = _tracking_nodes.get(node.get_instance_id())
+	if !net_node:
+		return
+
+	for peer_id: int in net_node.peers_vision:
+		_on_peer_lost_vision(node, net_node, peer_id)
+	
+	_release_network_id(node, net_node.network_id)
+	_tracking_nodes.erase(node.get_instance_id())
+
+## called only on owner side
+func _on_peer_got_vision(node: Node, net_node: _NetworkNodeInfo, peer_id: int) -> void:
+	_rpc_spawn.rpc_id(peer_id, node.scene_file_path, node.name, net_node.network_id, null)
+
+## called only on owner side
+func _on_peer_lost_vision(_node: Node, net_node: _NetworkNodeInfo, peer_id: int) -> void:
+	_rpc_despawn.rpc_id(peer_id, net_node.network_id, null)
+
+func _try_get_auto_spawnable_scene(node: Node) -> PackedScene:
+	for scene: PackedScene in auto_spawnable_scenes:
+		if scene.resource_path == node.scene_file_path:
+			return scene
+
+	return null
+
+func _find_node_id_by_network_id(network_id: int) -> int:
+	for node_id: int in _tracking_nodes.keys():
+		if _tracking_nodes[node_id].network_id == network_id:
+			return node_id
+
+	return 0
+
+func _alloc_network_id(node: Node) -> int:
+	return node.get_instance_id()
+
+func _release_network_id(_node: Node, _network_id: int) -> void:
+	pass
+
+@rpc("reliable")
+func _rpc_spawn(scene_path: String, node_name: String, network_id: int, data: Variant) -> void:
+	var node: Node
+	if data:
+		node = spawn_function.call(data)
+		if !node:
+			printerr("spawn_function doesn't returned node")
+			return
+	else:
+		node = (load(scene_path) as PackedScene).instantiate()
+
+	node.name = node_name
+
+	var net_node: _NetworkNodeInfo = _NetworkNodeInfo.new(network_id)
+	_tracking_nodes[node.get_instance_id()] = net_node
+
+	var spawn_target: Node = get_node(spawn_path)
+	if !spawn_target:
+		printerr("spawn_path pointing to invalid node")
+		return
+
+	spawn_target.add_child(node)
+	spawned.emit(node)
+
+@rpc("reliable")
+func _rpc_despawn(network_id: int) -> void:
+	var node_id: int = _find_node_id_by_network_id(network_id)
+	if !is_instance_id_valid(node_id):
+		printerr("authority sent despawn rpc with unknown node network_id")
+		return
+
+	_tracking_nodes.erase(node_id)
+	var node: Node = instance_from_id(node_id)
+	if !is_instance_valid(node):
+		printerr("got despawn rpc but local node instance is invalid")
+		return
+
+	despawning.emit(node)
+	node.queue_free()
+
+func _erase_replacing(arr: Array, index: int) -> void:
+	if index < 0 or index >= arr.size():
+		return
+
+	if arr.size() == 1:
+		arr.clear()
+		return
+
+	var last_idx: int = arr.size() - 1
+	if index < last_idx:
+		arr[index] = arr[last_idx]
+	arr.resize(last_idx)
+
+class _NetworkNodeInfo:
+	var network_id: int
+	var peers_vision: Array[int] = []
+
+	func _init(net_id: int) -> void:
+		self.network_id = net_id
