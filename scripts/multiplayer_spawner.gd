8bd2ed80c4fe7c2cf347dab2790676aa7ab7a756
AleD93 <adworld.aled@gmail.com>
2025-10-14 23:22:30 +0900

    Fix network vision incomplete setup

--- scripts/multiplayer_spawner.gd
+++ scripts/multiplayer_spawner.gd
@@ -18,7 +18,12 @@ func spawn_player(id: int) -> void:
 	player.position.y = 0
 	player.position.z = randf_range(-5, 5)
 	get_node(spawn_path).call_deferred("add_child", player)
-	(player.get_node("NetworkVisibilityArea") as NetworkVision).spawner = self
+
+	var net_vis: NetworkVision = player.get_node("NetworkVisibilityArea") as NetworkVision
+	if net_vis:
+		net_vis.spawner = self
+		net_vis.vision_owner_peer_id = id
+		
 	set_visibility_for(id, player, true)
 
 func despawn_player(id: int) -> void:
