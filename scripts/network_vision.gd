b49332687e00923c4d0e2a0a59f14524dee5f925
AleD93 <adworld.aled@gmail.com>
2025-10-14 13:14:39 +0900

    Add script NetworkVision to test network visibility

---
+++ scripts/network_vision.gd
@@ -0,0 +1,35 @@
+class_name NetworkVision
+extends Node
+
+@export var vision_radius: float = 3.0
+@export var spawner: AdvancedMultiplayerSpawner
+@export var vision_area: Area3D
+@export var vision_owner_peer_id: int
+
+func _enter_tree() -> void:
+	if vision_area:
+		vision_area.body_entered.connect(_on_body_entered)
+		vision_area.body_exited.connect(_on_body_exited)
+
+func _exit_tree() -> void:
+	if vision_area:
+		vision_area.body_entered.disconnect(_on_body_entered)
+		vision_area.body_exited.disconnect(_on_body_exited)
+
+func _on_body_entered(body: Node3D) -> void:
+	if !is_multiplayer_authority():
+		return
+
+	var node: Node = body
+	if node and !node.scene_file_path.is_empty():
+		print("entered [%s] %s (%d)" % [node.get_class(), node.name, node.get_instance_id()])
+		spawner.set_visibility_for(vision_owner_peer_id, node, true)
+
+func _on_body_exited(body: Node3D) -> void:
+	if !is_multiplayer_authority():
+		return
+
+	var node: Node = body
+	if node and !node.scene_file_path.is_empty():
+		print("exited [%s] %s (%d)" % [node.get_class(), node.name, node.get_instance_id()])
+		spawner.set_visibility_for(vision_owner_peer_id, node, false)
