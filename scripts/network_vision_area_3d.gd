f1632077b00303ad4fa23bd52e3ff59f515b8b03
AleD93 <adworld.aled@gmail.com>
2025-10-19 19:03:21 +0900

    Clarify network vision nodes naming

--- scripts/network_vision_area_3d.gd
+++ scripts/network_vision_area_3d.gd
@@ -1,23 +1,20 @@
-class_name NetworkVision
-extends Node
+class_name NetworkVisionArea3D
+extends Area3D
 
 @export var vision_radius: float = 3.0
-@export var vision_area: Area3D
 @export var vision_owner_peer_id: int
 
 func _enter_tree() -> void:
-	if vision_area:
-		vision_area.body_entered.connect(_on_body_entered)
-		vision_area.body_exited.connect(_on_body_exited)
-		vision_area.area_entered.connect(_on_area_entered)
-		vision_area.area_exited.connect(_on_area_exited)
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
 
 func _exit_tree() -> void:
-	if vision_area:
-		vision_area.body_entered.disconnect(_on_body_entered)
-		vision_area.body_exited.disconnect(_on_body_exited)
-		vision_area.area_entered.disconnect(_on_area_entered)
-		vision_area.area_exited.disconnect(_on_area_exited)
+	body_entered.disconnect(_on_body_entered)
+	body_exited.disconnect(_on_body_exited)
+	area_entered.disconnect(_on_area_entered)
+	area_exited.disconnect(_on_area_exited)
 
 func _on_body_entered(body: Node3D) -> void:
 	if !is_multiplayer_authority():
