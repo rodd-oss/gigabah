1f105c962e4d3a4298fd27f90429047d09393f46
AleD93 <adworld.aled@gmail.com>
2025-10-25 17:23:05 +0900

    Add hit area to bullet

--- components/network_projectile.gd
+++ components/network_projectile.gd
@@ -4,8 +4,19 @@ class_name NetworkProjectile
 
 @export var speed: float = 1.0
 @export var move_direction: Vector3 = Vector3.FORWARD
+@export var damage: int = 1
+@export var hit_area: Area3D
+@export var destroy_on_hitbox_hit: bool = true
 
 @onready var parent: CharacterBody3D = get_parent() as CharacterBody3D
+
+signal entered_hitbox(hitbox: HitBox3D)
+signal leaved_hitbox(hitbox: HitBox3D)
+
+
+func _ready() -> void:
+	if hit_area and multiplayer.is_server():
+		hit_area.area_entered.connect(_on_hit_area_area_entered)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,4 +31,15 @@ func _physics_process(_delta: float) -> void:
 
 func _on_decay_timer_timeout() -> void:
 	if multiplayer.is_server():
+		owner.queue_free()
+
+
+func _on_hit_area_area_entered(other: Area3D) -> void:
+	var hitbox_area := other as HitBox3D
+	if not hitbox_area:
+		return
+	
+	hitbox_area.hp.take_damage(damage)
+
+	if destroy_on_hitbox_hit:
 		owner.queue_free()
