bf2ad4b3983a962d41d57599fd3809398a22178d
AleD93 <adworld.aled@gmail.com>
2025-10-25 16:31:51 +0900

    Initial ability system

---
+++ scripts/ability.gd
@@ -0,0 +1,129 @@
+@abstract
+class_name Ability
+extends Node
+
+var caster: Caster
+var cooldown: float:
+	set(val):
+		var started := cooldown == 0.0 and val > 0.0
+		cooldown = max(0.0, val)
+		if started:
+			cooldown_start.emit()
+
+## When cooldown value changed from zero to positive value
+signal cooldown_start()
+## When cooldown value become zero
+signal cooldown_end()
+signal start_casting()
+signal succesfully_casted()
+
+
+@abstract func _get_cast_method() -> CastMethod
+
+
+## Overridable, don't call super it's placeholder
+func _cast_notarget() -> CastError:
+	return CastError.ABILITY_NOT_NOTARGET
+
+
+## Overridable, don't call super it's placeholder
+# func _cast_targeted(_target: Node) -> CastError:
+# 	return CastError.ABILITY_NOT_TARGETED
+
+
+## Overridable, don't call super it's placeholder
+func _cast_in_direction(_dir: Vector3) -> CastError:
+	return CastError.ABILITY_NOT_DIRECTIONAL
+
+
+func cast_notarget() -> CastError:
+	var err := _is_castable()
+	if err:
+		return err
+
+	_pre_cast()
+
+	err = _cast_notarget()
+	if err:
+		return err
+
+	_post_cast()
+
+	return CastError.OK
+
+
+# func cast_targeted(target: Node) -> CastError:
+# 	var err := _is_castable()
+# 	if err:
+# 		return err
+
+# 	_pre_cast()
+	
+# 	err = _cast_targeted(target)
+# 	if err:
+# 		return err
+
+# 	_post_cast()
+
+# 	return CastError.OK
+
+
+func cast_in_direction(dir: Vector3) -> CastError:
+	var err := _is_castable()
+	if err:
+		return err
+
+	_pre_cast()
+
+	err = _cast_in_direction(dir)
+	if err:
+		return err
+
+	_post_cast()
+
+	return CastError.OK
+
+
+func _is_castable() -> CastError:
+	if not caster:
+		return CastError.NO_CASTER
+
+	if cooldown > 0.0:
+		return CastError.ON_COOLDOWN
+
+	return CastError.OK
+
+
+func _pre_cast() -> void:
+	caster._cast_started(self)
+	start_casting.emit()
+
+
+func _post_cast() -> void:
+	caster._cast_done(self)
+	succesfully_casted.emit()
+
+
+func _process(delta: float) -> void:
+	if cooldown > delta:
+		cooldown -= delta
+	else:
+		cooldown = 0.0
+		cooldown_end.emit()
+
+
+enum CastError {
+	OK,
+	NO_CASTER,
+	ON_COOLDOWN,
+	ABILITY_NOT_NOTARGET,
+	ABILITY_NOT_TARGETED,
+	ABILITY_NOT_DIRECTIONAL,
+	TARGET_IS_FAR,
+}
+
+enum CastMethod {
+	NO_TARGET,
+	# TARGETED,
+	DIRECTIONAL,
+}
---
+++ scripts/caster.gd
@@ -0,0 +1,83 @@
+class_name Caster
+extends Node3D
+
+## Where ability nodes will store. Also ability nodes added to this container
+## will be automatically added to this caster.
+@export var abilities_container: NodePath = "."
+
+
+signal ability_added(ability_index: int)
+signal ability_removed(ability: Ability)
+## Emits when all basic checks done but before calling ability's cast code
+signal start_casting(ability: Ability)
+## Emits after ability's cast code executed and it returned OK
+signal successfully_casted(ability: Ability)
+
+var _abilities: Array[Ability] = []
+@onready var _container_node: Node = get_node(abilities_container)
+
+
+func get_ability_count() -> int:
+	return _abilities.size()
+
+
+func get_ability(index: int) -> Ability:
+	if index >= 0 and index < _abilities.size():
+		return _abilities[index]
+	return null
+
+
+func add_ability(ability: Ability) -> CasterError:
+	if not ability:
+		return CasterError.ABILITY_IS_NULL
+
+	if ability in _abilities:
+		return CasterError.ALREADY_HAVE_THIS_ABILITY
+
+	_abilities.append(ability)
+	ability.caster = self
+
+	ability_added.emit(_abilities.size() - 1)
+
+	return CasterError.OK
+
+
+func remove_ability(ability_index: int) -> CasterError:
+	if ability_index < 0 or ability_index >= _abilities.size():
+		return CasterError.ABILITY_INDEX_OF_OUT_BOUNDS
+
+	var ability := _abilities[ability_index]
+
+	ability.caster = null
+	_abilities.remove_at(ability_index)
+
+	ability_removed.emit(ability)
+
+	return CasterError.OK
+
+
+func _ready() -> void:
+	if _container_node:
+		for node: Node in _container_node.get_children():
+			if node is not Ability:
+				continue
+
+			var err := add_ability(node as Ability)
+			if err:
+				push_error("failed adding ability: %s" % CasterError.keys()[err])
+
+
+func _cast_started(ability: Ability) -> void:
+	start_casting.emit(ability)
+
+
+func _cast_done(ability: Ability) -> void:
+	successfully_casted.emit(ability)
+
+
+enum CasterError {
+	OK,
+	ABILITY_IS_NULL,
+	ABILITY_INDEX_OF_OUT_BOUNDS,
+	ALREADY_HAVE_THIS_ABILITY,
+}
