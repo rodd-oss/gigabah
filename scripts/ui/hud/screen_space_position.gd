e91926ec8b2cc9337d02f1eb1fc2e86d7919e4eb
AleD93 <adworld.aled@gmail.com>
2025-10-26 02:22:20 +0900

    Add node which sync screen space position of canvas layer and global position of itself

---
+++ scripts/ui/hud/screen_space_position.gd
@@ -0,0 +1,14 @@
+## This node updates canvas_layer's offset to match this node's
+## 3D position in screen space
+class_name ScreenSpacePosition
+extends Node3D
+
+@export var canvas_layer: CanvasLayer
+
+func _process(_delta: float) -> void:
+	var camera := get_viewport().get_camera_3d()
+	if not camera or not canvas_layer:
+		return
+
+	var pos_on_viewport := camera.unproject_position(global_position)
+	canvas_layer.offset = pos_on_viewport
