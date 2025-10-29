8f15b14755858558f293560863ff5378b7b9a93e
AleD93 <adworld.aled@gmail.com>
2025-10-29 22:47:07 +0900

    Add animation tree to player, network sync of player animation tree

--- scripts/animations_base.gd
+++ scripts/animations_base.gd
@@ -1,19 +1,61 @@
 extends Node
 
+## Speed (in m/s) of walking animation when foots on floor not slides
+const WALK_ANIM_SPEED = 5.0
+
 @export var input_controller: InputController
-@onready var player_model: Node3D = $"../PlayerModel"
-@onready var animation_player: AnimationPlayer = $"../PlayerModel/AnimationPlayer"
+@export var hero: Hero
+@export var caster: Caster
+@export var model_root: Node3D
+@export var animation_tree: AnimationTree
 
 
-# Animation: TEST Should be replaced with proper script in future!!!
+func _ready() -> void:
+	hero.jumped.connect(_on_hero_jumped)
+	hero.landed.connect(_on_hero_landed)
+
+	caster.start_casting.connect(_on_caster_start_casting)
+
+
 func _process(_delta: float) -> void:
 	if !multiplayer.is_server():
 		return
 
-	animation_player.speed_scale = 1.0
-	if input_controller.move_direction == Vector2.ZERO:
-		animation_player.play("Idle")
-	else:
-		animation_player.speed_scale = 5.0 / 2
-		animation_player.play("Walk")
-	#player_model.look_at(input_controller.global_position + input_controller.velocity, Vector3.UP)
+	if not input_controller.move_direction.is_zero_approx():
+		var new_rot := model_root.rotation
+		new_rot.y = -input_controller.move_direction.angle() - PI * 0.5
+		model_root.rotation = new_rot
+
+	animation_tree.set(
+		&"parameters/Alive/BodyBottomGraph/WalkBlend/blend_position",
+		hero.velocity.length() / WALK_ANIM_SPEED,
+	)
+	animation_tree.set(
+		&"parameters/Alive/BodyBottomGraph/conditions/grounded",
+		hero.is_on_floor(),
+	)
+
+
+func _on_hero_jumped() -> void:
+	animation_tree.set(
+		&"parameters/Alive/BodyBottomGraph/conditions/jumped",
+		true,
+	)
+
+
+func _on_hero_landed() -> void:
+	animation_tree.set(
+		&"parameters/Alive/BodyBottomGraph/conditions/jumped",
+		false,
+	)
+
+
+func _on_caster_start_casting(_ability: Ability) -> void:
+	animation_tree.set(
+		&"parameters/Alive/UpperBodyCastAnim/transition_request",
+		"Cast1",
+	)
+	animation_tree.set(
+		&"parameters/Alive/UpperBodyCasts/request",
+		AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE,
+	)
