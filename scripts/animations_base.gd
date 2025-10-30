c919bd2cf1d82bf59e69f1e08f0fda4202a871c4
AleD93 <adworld.aled@gmail.com>
2025-10-30 18:29:05 +0900

    Use RPC to play one shot animations

--- scripts/animations_base.gd
+++ scripts/animations_base.gd
@@ -11,6 +11,9 @@ const WALK_ANIM_SPEED = 5.0
 
 
 func _ready() -> void:
+	if !multiplayer.is_server():
+		return
+
 	hero.jumped.connect(_on_hero_jumped)
 	hero.landed.connect(_on_hero_landed)
 
@@ -51,11 +54,32 @@ func _on_hero_landed() -> void:
 
 
 func _on_caster_start_casting(_ability: Ability) -> void:
-	animation_tree.set(
+	_set_tree_param(
 		&"parameters/Alive/UpperBodyCastAnim/transition_request",
-		"Cast1",
+		"Cast1" if Time.get_ticks_msec() % 2 == 0 else "Cast2",
 	)
-	animation_tree.set(
+	_set_tree_param(
 		&"parameters/Alive/UpperBodyCasts/request",
 		AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE,
 	)
+
+
+## intended to use to request one shot animations because NetworkSynchronizer
+## can skip window when `*/request` is set and before it unsets by tree
+func _set_tree_param(param: StringName, value: Variant) -> void:
+	var spawner_inst_id: Variant = owner.get_meta(AdvancedMultiplayerSpawner.META_ADVANCED_SPAWNER)
+	if spawner_inst_id == null:
+		push_error("no advanced spawner meta for node %s" % owner)
+		return
+
+	# TODO: little hack until issue #92 will be fixed
+	#       https://github.com/rodd-oss/gigabah/issues/92
+	var spawner := instance_from_id(spawner_inst_id) as AdvancedMultiplayerSpawner
+	for i: int in range(spawner.get_peers_have_vision_count(owner)):
+		var peer_id := spawner.get_peer_have_vision(owner, i)
+		_rpc_set_tree_param.rpc_id(peer_id, param, value)
+
+
+@rpc("authority", "reliable", "call_local")
+func _rpc_set_tree_param(param: StringName, value: Variant) -> void:
+	animation_tree.set(param, value)
