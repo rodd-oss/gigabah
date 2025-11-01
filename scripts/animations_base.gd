f04f269852ced11a49088de76faa41e478e42a30
AleD <adworld.aled@gmail.com>
2025-11-01 18:24:28 +0900

    Visuals (#93)
    * Add animation tree to player, network sync of player animation tree
    
    * Add one shot script for emitters only scenes
    
    * Replace bullet model from sphere to projectile with vfx
    
    * Add impact effect to auto spawn list
    
    * Use RPC to play one shot animations
    
    * Make projectile look along its path, spawn impact effect
    
    * Add signals for jumped and landed events
    
    * Generalize projectile script by moving logic into ability

--- scripts/animations_base.gd
+++ scripts/animations_base.gd
@@ -1,19 +1,85 @@
 extends Node
 
+## Speed (in m/s) of walking animation when foots on floor not slides
+const WALK_ANIM_SPEED = 5.0
+
 @export var input_controller: InputController
-@onready var player_model: Node3D = $"../PlayerModel"
-@onready var animation_player: AnimationPlayer = $"../PlayerModel/AnimationPlayer"
+@export var hero: Hero
+@export var caster: Caster
+@export var model_root: Node3D
+@export var animation_tree: AnimationTree
 
 
-# Animation: TEST Should be replaced with proper script in future!!!
+func _ready() -> void:
+	if !multiplayer.is_server():
+		return
+
+	hero.jumped.connect(_on_hero_jumped)
+	hero.landed.connect(_on_hero_landed)
+
+	caster.start_casting.connect(_on_caster_start_casting)
+
+
 func _process(_delta: float) -> void:
 	if !multiplayer.is_server():
 		return
 
-	animation_player.speed_scale = 1.0
-	if input_controller.move_direction == Vector2.ZERO:
-		animation_player.play("Idle")
-	else:
-		animation_player.speed_scale = 5.0 / 2
-		animation_player.play("Walk")
-	#player_model.look_at(input_controller.global_position + input_controller.velocity, Vector3.UP)
+	if not input_controller.move_direction.is_zero_approx():
+		var new_rot := model_root.rotation
+		new_rot.y = -input_controller.move_direction.angle() - PI * 0.5
+		model_root.rotation = new_rot
+
+	animation_tree.set(
+		&"parameters/Alive/BodyBottomGraph/WalkBlend/blend_position",
+		hero.velocity.length() / WALK_ANIM_SPEED,
+	)
+	animation_tree.set(
+		&"parameters/Alive/BodyBottomGraph/conditions/grounded",
+		hero.is_on_floor(),
+	)
+
+
+func _on_hero_jumped() -> void:
+	animation_tree.set(
+		&"parameters/Alive/BodyBottomGraph/conditions/jumped",
+		true,
+	)
+
+
+func _on_hero_landed() -> void:
+	animation_tree.set(
+		&"parameters/Alive/BodyBottomGraph/conditions/jumped",
+		false,
+	)
+
+
+func _on_caster_start_casting(_ability: Ability) -> void:
+	_set_tree_param(
+		&"parameters/Alive/UpperBodyCastAnim/transition_request",
+		"Cast1" if Time.get_ticks_msec() % 2 == 0 else "Cast2",
+	)
+	_set_tree_param(
+		&"parameters/Alive/UpperBodyCasts/request",
+		AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE,
+	)
+
+
+## intended to use to request one shot animations because NetworkSynchronizer
+## can skip window when `*/request` is set and before it unsets by tree
+func _set_tree_param(param: StringName, value: Variant) -> void:
+	var spawner_inst_id: Variant = owner.get_meta(AdvancedMultiplayerSpawner.META_ADVANCED_SPAWNER)
+	if spawner_inst_id == null:
+		push_error("no advanced spawner meta for node %s" % owner)
+		return
+
+	# TODO: little hack until issue #92 will be fixed
+	#       https://github.com/rodd-oss/gigabah/issues/92
+	var spawner := instance_from_id(spawner_inst_id) as AdvancedMultiplayerSpawner
+	for i: int in range(spawner.get_peers_have_vision_count(owner)):
+		var peer_id := spawner.get_peer_have_vision(owner, i)
+		_rpc_set_tree_param.rpc_id(peer_id, param, value)
+
+
+@rpc("authority", "reliable", "call_local")
+func _rpc_set_tree_param(param: StringName, value: Variant) -> void:
+	animation_tree.set(param, value)
--- scripts/components/network_projectile.gd
+++ scripts/components/network_projectile.gd
@@ -4,19 +4,24 @@ class_name NetworkProjectile
 
 @export var speed: float = 1.0
 @export var move_direction: Vector3 = Vector3.FORWARD
-@export var damage: int = 1
 @export var hit_area: Area3D
-@export var destroy_on_hitbox_hit: bool = true
+@export var move_controller: CharacterBody3D
 
 @onready var parent: CharacterBody3D = get_parent() as CharacterBody3D
 
+## Emits when hit area of projectile enters someone's hitbox
 signal entered_hitbox(hitbox: HitBox3D)
+## Emits when hit area of projectile leaves someone's hitbox
 signal leaved_hitbox(hitbox: HitBox3D)
+## Emits when hits regular solid collider
+signal hit_wall(collision: KinematicCollision3D)
 
 
 func _ready() -> void:
-	if hit_area and multiplayer.is_server():
-		hit_area.area_entered.connect(_on_hit_area_area_entered)
+	if multiplayer.is_server():
+		if hit_area:
+			hit_area.area_entered.connect(_on_hit_area_area_entered)
+			hit_area.area_exited.connect(_on_hit_area_area_exited)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,7 +31,9 @@ func _process(_delta: float) -> void:
 
 func _physics_process(_delta: float) -> void:
 	if multiplayer.is_server():
-		parent.move_and_collide(move_direction * speed * _delta)
+		var collision := move_controller.move_and_collide(move_direction * speed * _delta)
+		if collision:
+			hit_wall.emit(collision)
 
 
 func _on_decay_timer_timeout() -> void:
@@ -39,7 +46,12 @@ func _on_hit_area_area_entered(other: Area3D) -> void:
 	if not hitbox_area:
 		return
 
-	hitbox_area.hp.take_damage(damage)
+	entered_hitbox.emit(hitbox_area)
 
-	if destroy_on_hitbox_hit:
-		owner.queue_free()
+
+func _on_hit_area_area_exited(other: Area3D) -> void:
+	var hitbox_area := other as HitBox3D
+	if not hitbox_area:
+		return
+
+	leaved_hitbox.emit(hitbox_area)
